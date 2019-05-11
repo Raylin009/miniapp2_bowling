fb20687dd41166f02de2072e149ef6b185c0cbd7
Ray Lin <raylin009@gmail.com>
2019-05-11 11:20:58 -0700

    Setup: Component layout rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -8,7 +9,21 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>App Connected</div>
+      <div>
+        <div className="header">
+          <h2>Bowling App</h2>
+          <h4> brought to you by Ray Lin</h4>
+        </div>
+        <div>
+          <h5>Score Card</h5>
+          <h6>Frams</h6>
+          <h6>LastFram</h6>
+        </div>
+        <div className="scoreInput">
+          <h5>KeyPad</h5>
+          <h6>Keys</h6>
+        </div>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-const Index = () => {
-  return <div>Hello React!</div>;
-};
-
 ReactDOM.render(<App />, document.getElementById('app'));
